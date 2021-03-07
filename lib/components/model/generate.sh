e63dedaac19a93ad77a107a3eef72aafe3ffd624
EB <elayabharath@gmail.com>
2021-03-07 20:04:18 +0530

    Moved generate model from nook
    + Added generation script under components/model
    + Updated readme with model generation script

---
+++ lib/components/model/generate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+WORKDIR="$(pwd)"
+
+# get absolute paths
+PROJDIR="$(pwd)"
+OUTDIR="$PROJDIR/lib/components/model"
+
+cd ".."
+LARKDIR="$(pwd)"
+
+FIREBASEDIR="$LARKDIR/Katikati-PyLib/katikati_pylib/firebase"
+CODEGEN="$LARKDIR/Infrastructure/tool/codegen"
+
+cd "$CODEGEN"
+
+# generate the file(s)
+# add the following line to generate python export script
+#   --export-script "$FIREBASEDIR/export_nook_model.g.py" \
+dart bin/generate_firebase_model.dart \
+    "$OUTDIR/model.yaml"
+
+# assert that the generated file is valid Dart
+dartanalyzer --packages "$PROJDIR/webapp/.packages" "$OUTDIR/model.g.dart"
