f8a5a7fb49741162545fc827f0d74cd416a4e76b
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2021-05-20 15:20:32 +0100

    Rename suggested tags to use snake_case

--- lib/components/model/model.g.dart
+++ lib/components/model/model.g.dart
@@ -75,7 +75,7 @@ class Conversation {
     return (modelObj ?? Conversation())
       ..demographicsInfo = Map_fromData<String>(data['demographicsInfo'], String_fromData)
       ..tagIds = Set_fromData<String>(data['tags'], String_fromData)
-      ..suggestedTagIds = Set_fromData<String>(data['suggestedTags'], String_fromData) ?? {}
+      ..suggestedTagIds = Set_fromData<String>(data['suggested_tag_ids_set'], String_fromData) ?? {}
       ..lastInboundTurnTagIds = Set_fromData<String>(data['lastInboundTurnTags'], String_fromData) ?? {}
       ..messages = List_fromData<Message>(data['messages'], Message.fromData)
       ..notes = String_fromData(data['notes'])
@@ -104,7 +104,7 @@ class Conversation {
     return {
       if (demographicsInfo != null) 'demographicsInfo': demographicsInfo,
       if (tagIds != null) 'tags': tagIds.toList(),
-      if (suggestedTagIds != null) 'suggestedTags': suggestedTagIds.toList(),
+      if (suggestedTagIds != null) 'suggested_tag_ids_set': suggestedTagIds.toList(),
       if (lastInboundTurnTagIds != null) 'lastInboundTurnTags': lastInboundTurnTagIds.toList(),
       if (messages != null) 'messages': messages.map((elem) => elem?.toData()).toList(),
       if (notes != null) 'notes': notes,
@@ -201,7 +201,7 @@ class Message {
   DateTime datetime;
   MessageStatus status;
   List<String> tagIds;
-  List<String> suggestedTagIds;
+  Set<String> suggestedTagIds;
   String text;
   String translation;
   String id;
@@ -213,7 +213,7 @@ class Message {
       ..datetime = DateTime_fromData(data['datetime'])
       ..status = MessageStatus.fromData(data['status'])
       ..tagIds = List_fromData<String>(data['tags'], String_fromData)
-      ..suggestedTagIds = List_fromData<String>(data['suggestedTags'], String_fromData) ?? []
+      ..suggestedTagIds = Set_fromData<String>(data['suggested_tag_ids_set'], String_fromData) ?? {}
       ..text = String_fromData(data['text'])
       ..translation = String_fromData(data['translation'])
       ..id = String_fromData(data['id']);
@@ -239,7 +239,7 @@ class Message {
       if (datetime != null) 'datetime': datetime.toIso8601String(),
       if (status != null) 'status': status.toData(),
       if (tagIds != null) 'tags': tagIds,
-      if (suggestedTagIds != null) 'suggestedTags': suggestedTagIds,
+      if (suggestedTagIds != null) 'suggested_tag_ids_set': suggestedTagIds.toList(),
       if (text != null) 'text': text,
       if (translation != null) 'translation': translation,
       if (id != null) 'id': id,
