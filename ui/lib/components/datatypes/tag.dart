f46ad2ed680b07bd44efc53c854a5e9ed6f95aef
danrubel <dan.rubel.usa@gmail.com>
2021-06-29 14:12:31 -0400

    new Tag and TagType model classes

---
+++ ui/lib/components/datatypes/tag.dart
@@ -0,0 +1,6 @@
+/// This is the interface file for tag.g.dart
+/// Import this file rather than importing tag.g.dart directly
+
+export 'package:katikati_ui_lib/components/datatypes/tag.g.dart';
+
+// Add hand coded adjustments and modifications here rather than modifying the generated file.
