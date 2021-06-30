e38faf18845443191a0e32380567f1c33b67e28b
Elayabharath <elayabharath@gmail.com>
2021-06-30 14:23:20 +0530

    Added accordion, refactored editable text along with event propagation behaviour (#55)
    * Added accordion, refactored editable text along with event propagation behaviour
    
    * Update editable_text.dart
    
    * PR fixes

---
+++ ui/lib/components/accordion/accordion.dart
@@ -0,0 +1,150 @@
+import 'dart:html';
+
+/*
+<div class="accordion">
+  <div class="accordion-item">
+    <div class="accordion-item__header">
+      <span class="accordion-indicator">
+        <icon />
+      </span>
+      <header />
+    </div>
+    <div class="accordion-body__wrapper">
+      <body />
+    </div>
+  </div>
+  ...
+</div>
+*/
+
+const EXPAND_CSS_CLASSNAME = "fas fa-caret-right";
+const COLLAPSE_CSS_CLASSSNAME = "fas fa-caret-down";
+
+class AccordionItem {
+  String id;
+  bool _isOpen = false;
+  SpanElement _indicatorIcon;
+  DivElement headerElement;
+  DivElement bodyElement;
+  DivElement renderElement;
+
+  DivElement _headerWrapper;
+  DivElement _bodyWrapper;
+
+  AccordionItem(this.id, this.headerElement, this.bodyElement, this._isOpen, {String dataId}) {
+    _headerWrapper = DivElement()..className = 'accordion-item__header';
+    _indicatorIcon = SpanElement()..className = _isOpen ? COLLAPSE_CSS_CLASSSNAME : EXPAND_CSS_CLASSNAME;
+    var indicatorElement = SpanElement()
+      ..className = 'accordion-indicator'
+      ..append(_indicatorIcon)
+      ..onClick.listen((e) {
+        e.stopPropagation();
+        toggle();
+      });
+    _headerWrapper
+      ..append(indicatorElement)
+      ..append(headerElement)
+      ..onClick.listen((e) {
+        e.stopPropagation();
+        toggle();
+      });
+
+    _bodyWrapper = DivElement()
+      ..className = 'accordion-body__wrapper'
+      ..append(bodyElement);
+    if (!_isOpen) {
+      _bodyWrapper.classes.toggle("hidden", true);
+    }
+
+    renderElement = DivElement()
+      ..dataset['id'] = 'accordion-item-${id ?? ""}'
+      ..className = 'accordion-item'
+      ..append(_headerWrapper)
+      ..append(_bodyWrapper);
+  }
+
+  void expand() {
+    _indicatorIcon.className = COLLAPSE_CSS_CLASSSNAME;
+    _bodyWrapper.classes.toggle('hidden', false);
+    _isOpen = true;
+  }
+
+  void collapse() {
+    _indicatorIcon.className = EXPAND_CSS_CLASSNAME;
+    _bodyWrapper.classes.toggle("hidden", true);
+    _isOpen = false;
+  }
+
+  void toggle() {
+    _isOpen ? collapse() : expand();
+  }
+}
+
+class Accordion {
+  List<AccordionItem> _accordionItems;
+  bool _onlyOneOpen;
+  DivElement renderElement;
+
+  Accordion(this._accordionItems, {collapseAtStart: true, expandAtStart: false, onlyOneOpen: false}) {
+    _onlyOneOpen = onlyOneOpen;
+    renderElement = DivElement()..className = "accordion";
+    for (var item in _accordionItems) {
+      renderElement.append(item.renderElement);
+      if (collapseAtStart) {
+        item.collapse();
+      }
+      if (expandAtStart) {
+        item.expand();
+      }
+    }
+  }
+
+  AccordionItem queryItem(String id) {
+    return _accordionItems.firstWhere((item) => item.id == id, orElse: () => null);
+  }
+
+  void appendItem(AccordionItem item) {
+    _accordionItems.add(item);
+    renderElement.append(item.renderElement);
+  }
+
+  void removeItem(String id) {
+    _accordionItems.removeWhere((item) => item.id == id);
+    renderElement.children.removeWhere((item) => item.dataset['id'] == 'accordion-item-${id}');
+  }
+
+  void collapseAllItems() {
+    for (var item in _accordionItems) {
+      item.collapse();
+    }
+  }
+
+  void expandAllItems() {
+    for (var item in _accordionItems) {
+      item.expand();
+    }
+  }
+
+  void collapseItem(String id) {
+    for (var item in _accordionItems) {
+      if (item.id == id) {
+        item.collapse();
+      }
+    }
+  }
+
+  void expandItem(String id) {
+    for (var item in _accordionItems) {
+      if (item.id == id) {
+        item.expand();
+      } else if (_onlyOneOpen) {
+        item.collapse();
+      }
+    }
+  }
+
+  void clear() {
+    _accordionItems = [];
+    renderElement.children.clear();
+  }
+}
--- ui/lib/components/editable/editable_text.dart
+++ ui/lib/components/editable/editable_text.dart
@@ -1,131 +1,129 @@
 import 'dart:html';
-import 'dart:async';
-
 import 'package:katikati_ui_lib/components/button/button.dart';
-import 'package:katikati_ui_lib/components/logger.dart';
 
-var logger = Logger('TextEditableView');
+/*
+<span class="text-edit text-edit--editing">
+  <span class="text-edit__text" contentEditable></span>
+  <span class="text-edit__actions">
+      <button class="button button--text" title="">...</button>
+  </span>
+</span>
+*/
 
-class TextEditableView {
+class TextEdit {
+  SpanElement renderElement;
   String _text;
 
-  bool _editing = false;
+  bool _removable;
 
   SpanElement _textSpan;
+  SpanElement _textActions;
   Button _editButton;
-  Button _removeButton;
+  Button _deleteButton;
   Button _confirmButton;
-  Button _discardButton;
+  Button _cancelButton;
 
-  DivElement _renderElement;
-  DivElement get renderElement => _renderElement;
+  bool _isEditing = false;
 
-  Stream<String> _onChange;
-  StreamController<String> _onChangeController;
-  Stream<String> get onChange => _onChange;
+  void Function(String) onEdit = (_) {};
+  void Function() onDelete = () {};
+  void Function() onSelect = () {};
+  void Function() onAccept = () {};
+  void Function() onCancel = () {};
 
-  Stream<void> _onDelete;
-  StreamController<void> _onDeleteController;
-  Stream<void> get onDelete => _onDelete;
+  TextEdit(this._text, {bool removable = false}) {
+    _removable = removable;
 
-  TextEditableView(this._text, {bool editableOnAdd = false}) {
-    this._editing = editableOnAdd;
-    _renderElement = DivElement()
-      ..onMouseEnter.listen((_) {
-        if (!_editing) {
-          _editButton.show();
-          _removeButton.show();
-        }
-      })
-      ..onMouseLeave.listen((_) {
-        if (!_editing) {
-          _editButton.hide();
-          _removeButton.hide();
-        }
-      });
+    renderElement = SpanElement()..classes.add('text-edit');
 
     _textSpan = SpanElement()
-      ..className = "editable-text"
-      ..innerText = _text;
+      ..classes.add('text-edit__text')
+      ..dataset['placeholder'] = "untitled"
+      ..onClick.listen((e) {
+        if (!_isEditing) return;
+        e.stopPropagation();
+      })
+      ..onKeyDown.listen((event) {
+        if (!_isEditing) return;
+        if (event.keyCode == KeyCode.ENTER) {
+          event.preventDefault();
+          _confirmEdit();
+        }
+        if (event.keyCode == KeyCode.ESC) {
+          event.preventDefault();
+          _cancelEditing();
+        }
+      });
+    _textSpan.innerText = this._text;
 
-    _editButton = Button(ButtonType.edit, onClick: (_) {
-      _enableEdit();
+    _editButton = Button(ButtonType.edit, onClick: (e) {
+      e.stopPropagation();
+      beginEdit();
     });
-    _removeButton = Button(ButtonType.remove, onClick: (_) {
-      if (_onChangeController.hasListener) {
-        _onDeleteController.sink.add(null);
-      } else {
-        logger.warning("No listener for TextEditableView.onDelete");
-      }
-    });
-    _confirmButton = Button(ButtonType.confirm, onClick: (_) {
+    _confirmButton = Button(ButtonType.confirm, onClick: (e) {
+      e.stopPropagation();
       _confirmEdit();
     });
-    _discardButton = Button(ButtonType.remove, onClick: (_) {
-      _discardEdit();
+    _cancelButton = Button(ButtonType.cancel, onClick: (e) {
+      e.stopPropagation();
+      _cancelEditing();
+    });
+    _deleteButton = Button(ButtonType.remove, onClick: (e) {
+      e.stopPropagation();
+      _delete();
     });
 
-    _renderElement
-      ..append(_textSpan)
-      ..append(_editButton.renderElement)
-      ..append(_removeButton.renderElement)
-      ..append(_confirmButton.renderElement)
-      ..append(_discardButton.renderElement);
-
-    this._onChangeController = StreamController();
-    this._onChange = _onChangeController.stream;
-
-    this._onDeleteController = StreamController();
-    this._onDelete = _onDeleteController.stream;
-
-    if (!_editing) {
-      _editButton.hide();
-      _removeButton.hide();
-      _confirmButton.hide();
-      _discardButton.hide();
-    } else {
-      _enableEdit();
-      _textSpan.setAttribute('autofocus', 'true');
+    _textActions = SpanElement()..classes.add('text-edit__actions');
+    _textActions.append(_editButton.renderElement);
+    if (_removable) {
+      _textActions.append(_deleteButton.renderElement);
     }
+
+    renderElement..append(_textSpan)..append(_textActions);
   }
 
-  void _enableEdit() {
-    _editing = true;
-    _textSpan.setAttribute('contenteditable', 'true');
+  void beginEdit() {
+    _isEditing = true;
+    _textSpan.contentEditable = "true";
+    renderElement.classes.toggle("text-edit--editing", true);
+
+    _textActions.children.clear();
+    _textActions..append(_confirmButton.renderElement)..append(_cancelButton.renderElement);
+    focus();
+  }
+
+  void focus() {
     _textSpan.focus();
-
-    _editButton.hide();
-    _removeButton.hide();
-    _confirmButton.show();
-    _discardButton.show();
-  }
-
-  void _disableEdit() {
-    _editing = false;
-    _textSpan.removeAttribute('contenteditable');
-    _editButton.renderElement.removeAttribute('hidden');
-    _removeButton.renderElement.removeAttribute('hidden');
-
-    _editButton.show();
-    _removeButton.show();
-    _confirmButton.hide();
-    _discardButton.hide();
   }
 
   void _confirmEdit() {
-    _text = _textSpan.innerText;
-
-    if (_onChangeController.hasListener) {
-      _onChangeController.sink.add(_textSpan.text);
-    } else {
-      logger.warning("No listener for TextEditableView.onChange");
+    _resetActions();
+    if (_text != _textSpan.innerText) {
+      _text = _textSpan.innerText;
+      onEdit(_text);
     }
-
-    _disableEdit();
   }
 
-  void _discardEdit() {
+  void _cancelEditing() {
+    _resetActions();
     _textSpan.innerText = _text;
-    _disableEdit();
+    onCancel();
+  }
+
+  void _delete() {
+    _resetActions();
+    onDelete();
+  }
+
+  void _resetActions() {
+    _isEditing = false;
+    _textSpan..contentEditable = "false";
+    renderElement.classes.toggle("text-edit--editing", false);
+
+    _textActions.children.clear();
+    _textActions.append(_editButton.renderElement);
+    if (_removable) {
+      _textActions.append(_deleteButton.renderElement);
+    }
   }
 }
