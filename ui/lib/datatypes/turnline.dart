f72eca8b1e8c91dc36b0ceccea869cc028f423a8
danrubel <dan.rubel.usa@gmail.com>
2021-08-17 07:38:29 -0400

    move turnline model classes to new datatypes folder

--- ui/lib/datatypes/turnline.dart
+++ ui/lib/datatypes/turnline.dart
@@ -1,6 +1,6 @@
 /// This is the interface file for turnline.g.dart
 /// Import this file rather than importing turnline.g.dart directly
 
-export 'package:katikati_ui_lib/components/datatypes/turnline.g.dart';
+export 'package:katikati_ui_lib/datatypes/turnline.g.dart';
 
 // Add hand coded adjustments and modifications here rather than modifying the generated file.
