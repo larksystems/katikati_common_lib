0928f69163e79274c187a0166f8e83b00aac92d6
Elayabharath <elayabharath@gmail.com>
2021-09-19 20:41:54 +0800

    Update conversation item to carry date time on the summary, and an optional date time separator (#87)
    * Update conversation item to carry date time on the summary, and an optional date time separator
    
    * Add options to update text, date time
    
    * Update conversation_item.dart
    
    * PR fix
    
    * Delete dateTime.dart
    
    * Rename complete

---
+++ ui/lib/utils/datetime.dart
@@ -0,0 +1,19 @@
+import 'package:intl/intl.dart';
+
+String dateStringForSeparator(DateTime dateTime) {
+    final date = DateTime(dateTime.year, dateTime.month, dateTime.day);
+
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+
+    final differenceDays = today.difference(date).inDays;
+    final DateFormat formatter = DateFormat('MMM yyyy');
+
+    if (differenceDays < 1) {
+      return "Today";
+    } else if (differenceDays == 1) {
+      return "Yesterday";
+    } else {
+      return formatter.format(dateTime);
+    }
+  }
