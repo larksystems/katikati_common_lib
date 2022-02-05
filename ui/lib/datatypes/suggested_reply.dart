18a66285c4409cdcdd9a990ac2efab8de994d0b4
Elayabharath <elayabharath@gmail.com>
2022-02-05 17:06:02 +0800

    Clone method for SuggestedReply (#137)

--- ui/lib/datatypes/suggested_reply.dart
+++ ui/lib/datatypes/suggested_reply.dart
@@ -4,3 +4,10 @@
 export 'package:katikati_ui_lib/datatypes/suggested_reply.g.dart';
 
 // Add hand coded adjustments and modifications here rather than modifying the generated file.
+import 'package:katikati_ui_lib/datatypes/suggested_reply.g.dart' as g;
+
+extension SuggestedReplyUtils on g.SuggestedReply {
+  g.SuggestedReply clone() {
+    return g.SuggestedReply.fromData(this.toData())..docId = this.docId;
+  }  
+}
